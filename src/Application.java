ab14670806e5c77567bc34955c53aca7310ec76e
“agatarajewska <agatarajewska@gmail.com>
2017-12-29 18:45:39 +0100

    First Commit

---
+++ src/Application.java
@@ -0,0 +1,9 @@
+ class Application {
+
+    public static void main(String[] args) {
+
+        Kalkulator kalkulator = new Kalkulator();
+
+        int result = kalkulator.addAToB(5, 5);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+class Kalkulator {
+
+    public int addAToB (int a , int b){
+            return a + b;
+        }
+
+        public int subtractCToD (int c, int d) {
+            return c - d;
+        }
+
+
+    }
+
+
+
